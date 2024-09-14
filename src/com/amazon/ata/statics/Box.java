e8ec1603c38bd602e3029bc3d958c9f5da7b303d
issacdavis <issac.davis.1998@gmail.com>
2024-09-14 13:44:45 -0400

    tests passed

--- src/com/amazon/ata/statics/Box.java
+++ src/com/amazon/ata/statics/Box.java
@@ -1,7 +1,10 @@
 package com.amazon.ata.statics;
 
 public class Box {
-
+    private double height;
+    private double width;
+    private double length;
+    private static final double MAX_INCHES = 72;
     /**
      * Constructs a Box object if the dimensions are valid
      * @param length - length of box
@@ -10,7 +13,30 @@ public class Box {
      * @throws DimensionValueException if the provided dimensions exceed the max Box size
      */
     public Box(double length, double width, double height) throws DimensionValueException {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        if (!validateBoxDimensions(length, width, height)) {
+            throw new DimensionValueException("One of the values exceeds 72 inches.");
+        }
+    }
+    public double getHeight() {
+        return height;
+    }
 
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public static boolean validateBoxDimensions(double height, double width, double length) {
+        if (height > MAX_INCHES || width > MAX_INCHES || length > MAX_INCHES) {
+            return false;
+        }
+        return true;
     }
 
 }
